e63bf381b84f4eda5717d8160068942d43232797
zhouxuanhe <zhouxuanhe@aliyun.com>
2020-12-05 20:03:21 +0800

    优化

--- iOS_Tools/iOS_Tools/More/贝塞尔曲线/JJCuteView.m
+++ iOS_Tools/iOS_Tools/More/贝塞尔曲线/JJCuteView.m
@@ -12,23 +12,26 @@
 #define kControlMinHeight 100
 @interface JJCuteView ()<UITableViewDelegate,UITableViewDataSource>
 
+/// 模拟导航视图
 @property (nonatomic, strong) UIView *navView;
+/// 路径
 @property (nonatomic, strong) CAShapeLayer *shapeLayer;
-@property (nonatomic, strong) UIView *controlView;            // 切点,用Q表示
-@property (nonatomic, assign) CGFloat controlX;               // 切点x坐标
-@property (nonatomic, assign) CGFloat controlY;               // 切点y坐标
+/// 曲线路径控制点,为了更容易理解添加的. // 切点,用Q表示
+@property (nonatomic, strong) UIView *controlView;
+/// 切点位置
+@property (nonatomic, assign) CGPoint controlPoint;
+/// 定时器,为了做动画用
 @property (nonatomic, strong) CADisplayLink *displayLink;
+/// 记录当前是否在做动画
 @property (nonatomic, assign) BOOL isAnimating;
-@property (nonatomic, assign) CGFloat controlHeight;
-
-
+/// 列表
 @property (nonatomic, strong) JJTableView *tableView;
 
 @end
+
 @implementation JJCuteView
 
-static NSString *const kControlX = @"controlX";
-static NSString *const kControlY = @"controlY";
+static NSString *const kControlPoint = @"controlPoint";
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -43,18 +46,16 @@ static NSString *const kControlY = @"controlY";
 #pragma mark - 初始化界面
 - (void)setupUI{
     
-    [self addObserver:self forKeyPath:kControlX options:NSKeyValueObservingOptionNew context:nil];
-    [self addObserver:self forKeyPath:kControlY options:NSKeyValueObservingOptionNew context:nil];
-    
+    [self addObserver:self forKeyPath:kControlPoint options:NSKeyValueObservingOptionNew context:nil];
     
     // 手势
-//    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanAction:)];
-//    self.userInteractionEnabled = YES;
-//    [self addGestureRecognizer:pan];
+    // UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanAction:)];
+    // self.userInteractionEnabled = YES;
+    // [self addGestureRecognizer:pan];
     
     [self addSubview:self.tableView];
     [self.tableView.panGestureRecognizer addTarget:self action:@selector(handlePanAction:)];
-
+    
     self.navView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kControlMinHeight)];
     [self addSubview:self.navView];
     _shapeLayer = [CAShapeLayer layer];
@@ -65,83 +66,74 @@ static NSString *const kControlY = @"controlY";
     _displayLink.paused = YES;
     [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
     
-    
-    self.controlX = kScreenWidth/2.0;       // Q点x坐标
-    self.controlY = kControlMinHeight;            // Q点y坐标
-    _controlView = [[UIView alloc] initWithFrame:CGRectMake(_controlX, _controlY, 3, 3)];
+    // Q点坐标
+    self.controlPoint = CGPointMake(kScreenWidth/2.0, kControlMinHeight);
+    _controlView = [[UIView alloc] initWithFrame:CGRectMake(kScreenWidth/2.0, kControlMinHeight, 3, 3)];
     _controlView.backgroundColor = [UIColor redColor];
     [self addSubview:_controlView];
     
-    self.controlHeight = kControlMinHeight;
     _isAnimating = NO;
-    
 }
 
 - (void)handlePanAction:(UIPanGestureRecognizer *)pan{
     if (!_isAnimating) { //动画过程中不处理事件
-        
         if (pan.state == UIGestureRecognizerStateChanged){
-            
             CGPoint point = [pan translationInView:self];
             // 这部分代码使Q点跟着手势走
-            self.controlHeight = point.y*0.7 + kControlMinHeight;
-            self.controlX = kScreenWidth/2.0 + point.x;
-            self.controlY = self.controlHeight > kControlMinHeight ? self.controlHeight : kControlMinHeight;
-            self.controlView.frame = CGRectMake(self.controlX, self.controlY, self.controlView.frame.size.width, self.controlView.frame.size.height);
-            
+            CGFloat controlHeight = point.y*0.7 + kControlMinHeight;
+            CGFloat controlX = kScreenWidth/2.0 + point.x;
+            CGFloat controlY = controlHeight > kControlMinHeight ? controlHeight : kControlMinHeight;
+            self.controlPoint = CGPointMake(controlX, controlY);
+            self.controlView.frame = CGRectMake(controlX, controlY, self.controlView.frame.size.width, self.controlView.frame.size.height);
         }else if (pan.state == UIGestureRecognizerStateCancelled ||
                   pan.state == UIGestureRecognizerStateEnded ||
                   pan.state == UIGestureRecognizerStateFailed){
-
+            
             //手势结束,_shapeLayer昌盛产生弹簧效果
-             _isAnimating = YES;
-             _displayLink.paused = NO;           //开启displaylink,会执行方法calculatePath.
-
-             //弹簧
-             [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-                 self.controlView.frame = CGRectMake(kScreenWidth/2.0, kControlMinHeight, 3, 3);
-             } completion:^(BOOL finished) {
-                 if(finished){
-                     self.displayLink.paused = YES;
-                     self.isAnimating = NO;
-                 }
-             }];
-            
-            
+            _isAnimating = YES;
+            _displayLink.paused = NO;           //开启displaylink,会执行方法calculatePath.
+            //弹簧
+            [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+                self.controlView.frame = CGRectMake(kScreenWidth/2.0, kControlMinHeight, 3, 3);
+            } completion:^(BOOL finished) {
+                if(finished){
+                    self.displayLink.paused = YES;
+                    self.isAnimating = NO;
+                }
+            }];
         }
     }
 }
+
+/// 接收拖动代码也可以写在这里
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
     
 }
 
-
+//更新贝塞尔曲线图
 - (void)updateShapeLayerPath {
     // 更新_shapeLayer形状
     UIBezierPath *tPath = [UIBezierPath bezierPath];
     [tPath moveToPoint:CGPointMake(0, 0)];                              // A点
     [tPath addLineToPoint:CGPointMake(kScreenWidth, 0)];               // B点
     [tPath addLineToPoint:CGPointMake(kScreenWidth,  kControlMinHeight)];  // D点
-    [tPath addQuadCurveToPoint:CGPointMake(0, kControlMinHeight) controlPoint:CGPointMake(self.controlX, self.controlY)]; // C,D,Q确定的一个弧线
+    [tPath addQuadCurveToPoint:CGPointMake(0, kControlMinHeight) controlPoint:self.controlPoint]; // C,D,Q确定的一个弧线
     [tPath closePath];
     _shapeLayer.path = tPath.CGPath;
 }
 
+//KVO
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
-    if ([keyPath isEqualToString:kControlX] || [keyPath isEqualToString:kControlY]) {
+    if ([keyPath isEqualToString:kControlPoint]) {
         [self updateShapeLayerPath];
-         
     }
 }
 
 - (void)calculatePath{
     // 由于手势结束时,Q执行了一个UIView的弹簧动画,把这个过程的坐标记录下来,并相应的画出_shapeLayer形状
     CALayer *layer = self.controlView.layer.presentationLayer;
-    self.controlX = layer.position.x;
-    self.controlY = layer.position.y;
-    
+    self.controlPoint = CGPointMake(layer.position.x, layer.position.y);
 }
-
 
 #pragma mark -- TableView data source
 
@@ -157,22 +149,16 @@ static NSString *const kControlY = @"controlY";
     return 0;
 }
 
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
-    
     cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
-    
     return cell;
 }
-
-
-
 
 #pragma mark - lazy
 - (JJTableView *)tableView{
     if (_tableView == nil) {
-        _tableView = [[JJTableView alloc] initWithFrame:CGRectMake(0, kNavBarHeight, kScreenWidth, kScreenHeight-kNavBarHeight)];
+        _tableView = [[JJTableView alloc] initWithFrame:CGRectMake(0, kControlMinHeight, kScreenWidth, kScreenHeight-kControlMinHeight)];
         _tableView.delegate = self;
         _tableView.dataSource = self;
         _tableView.backgroundColor = [UIColor whiteColor];
